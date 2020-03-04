ad04cb51a7f6f4f3b93476d6c12d38d5f6618a01
Alex Barke <alex.barke@csintelligence.asia>
2020-03-04 14:10:16 +0800

    Update Home.js

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -9,6 +9,14 @@ export class Home extends Component {
         <h1>Hello, Veggie Maniacs!</h1>
         <p>Welcome to our inventory system for Veggies.</p>
         <p>Please click the Veggies link in the Navbar to get started</p>
+        <hr />
+        <h2>Seeding the database</h2>
+        <p>
+          If you haven't followed the steps on setting up the database and
+          seeding it with data, then you'll probably not be able to use this app
+          yet.
+        </p>
+        <p>Follow the instructions in the README</p>
       </div>
     );
   }
